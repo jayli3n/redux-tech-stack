83509eedb33fb8c40ed94ef5d93e54842c7f204d
Jay Li <jayli3n@gmail.com>
2019-06-03 11:58:48 +0800

    added reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends Component {
 	render() {
 		return (
-			<View />
-		)
+			<Provider store={createStore(reducers)}>
+				<View />
+			</Provider>
+		);
 	}
 }
 
